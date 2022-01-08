582b5342ec468f2f70b810a3f2b38f5af3202845
Emilio Calvo <emiliojosecalvo@gmail.com>
2022-01-08 23:59:31 +0000

    starter pack

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Game from "./Game";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello Emilio</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Game />
+      </div>
+    );
+  }
 }
 
 export default App;
